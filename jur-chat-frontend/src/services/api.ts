2fef220d5546246aa47bf0bf9078a1fdd86674b4
bernardolobomarques <bernardolobomarques@gmail.com>
2025-07-01 23:24:52 -0300

    feat: Encrypt username in login, register, getHistory, and uploadFile functions

--- jur-chat-frontend/src/services/api.ts
+++ jur-chat-frontend/src/services/api.ts
@@ -8,27 +8,31 @@ declare global {
 }
 
 export const login = async (username: string, password: string) => {
+  const encryptedUsername = hex_sha256(username);
   const encryptedPassword = hex_sha256(password);
   const response = await axios.post(`${N8N_WEBHOOK_URL}/b7c0c5d6-9835-4b26-b0ee-ccaf8829ca82`, {
-    username,
+    username: encryptedUsername,
     password: encryptedPassword,
   });
   return response.data;
 };
 
 export const register = async (username: string, email: string, password: string) => {
+  const encryptedUsername = hex_sha256(username);
+  const encryptedEmail = hex_sha256(email);
   const encryptedPassword = hex_sha256(password);
   const response = await axios.post(`${N8N_WEBHOOK_URL}/d746f1fe-5fab-49e7-894b-696aecb92a9d`, {
-    username,
-    email,
+    username: encryptedUsername,
+    email: encryptedEmail,
     password: encryptedPassword,
   });
   return response.data;
 };
 
 export const getHistory = async (username: string) => {
+  const encryptedUsername = hex_sha256(username);
   const response = await axios.post(`${N8N_WEBHOOK_URL}/c9eaf6ab-21bd-4817-8c7c-16b36019a113`, {
-    username,
+    username: encryptedUsername,
   });
   return response.data;
 };
@@ -41,9 +45,10 @@ export const getDocument = async (nome_documento: string) => {
 };
 
 export const uploadFile = async (file: File, username: string) => {
+  const encryptedUsername = hex_sha256(username);
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('username', username);
+  formData.append('username', encryptedUsername);
   formData.append('nome_documento', file.name);
   formData.append('is_file', 'true');
 
